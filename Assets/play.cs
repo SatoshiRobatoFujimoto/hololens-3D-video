70a503bfe7b3c6e0d3977127b8baa894af845211
DESKTOP-MF8LE16\Guillaume <guillaume@teillet.eu>
2016-10-09 12:23:28 +0200

    no message

---
+++ Assets/play.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play : MonoBehaviour
+{
+    public MovieTexture movTexture;
+    void Start()
+    {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+    }
+}
